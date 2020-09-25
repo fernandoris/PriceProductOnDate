91c5c2453c51222eebbe4db6eb83822bb79e9031
Fernando Rodríguez-Izquierdo Serrano <fernando.rodriguez.izquierdo.serrano@everis.com>
2020-09-25 20:36:08 +0200

    Initial commit

---
+++ src/main/java/com/priceproduct/ondate/OndateApplication.java
@@ -0,0 +1,13 @@
+package com.priceproduct.ondate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OndateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OndateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/priceproduct/ondate/OndateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.priceproduct.ondate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OndateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
