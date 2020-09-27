e6f4bf83247f7f6543441ecd90d1a84964494830
Fernando Rodríguez-Izquierdo Serrano <fernando.rodriguez.izquierdo.serrano@everis.com>
2020-09-27 20:09:14 +0200

    Add javadoc for OndateRespository

--- src/main/java/com/priceproduct/ondate/repository/OndateRepository.java
+++ src/main/java/com/priceproduct/ondate/repository/OndateRepository.java
@@ -13,6 +13,15 @@ import com.priceproduct.ondate.domain.Price;
 @Repository
 public interface OndateRepository extends JpaRepository<Price, Integer>, Serializable {
 
+	/**
+	 * Consult in the database the PRICES table filtering by productId, brandId and date. 
+	 * Sort the results by priority in descending order and return the first result
+	 * @param productId
+	 * @param brandId
+	 * @param date
+	 * @param date2
+	 * @return
+	 */
 	Optional<Price> findFirstByProductIdAndBrandIdAndStartDateBeforeAndEndDateAfterOrderByPriorityDesc(Integer productId,
 			Integer brandId, Date date, Date date2);
 
