7b5d57b27e6252b1db9af34294d622155a0f07ef
Fernando Rodríguez-Izquierdo Serrano <fernando@rodriguezizquierdo.com>
2020-09-28 12:16:55 +0200

    I implement the service with streams

--- src/main/java/com/priceproduct/ondate/service/impl/OndateServiceImpl.java
+++ src/main/java/com/priceproduct/ondate/service/impl/OndateServiceImpl.java
@@ -1,7 +1,11 @@
 package com.priceproduct.ondate.service.impl;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Optional;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
@@ -24,27 +28,47 @@ public class OndateServiceImpl implements OndateService {
 	
 	@Autowired
 	OndateMapper ondateMapper;
-	
-	@Override	
+
+	@Override
 	public Optional<OndateResponse> getPrice(Date date, Integer productId, Integer brandId) {
+		List<Price> priceList = ondateRepository.findAll();
 
-		OndateResponse response = null;
+		List<Predicate<Price>> allPredicates = new ArrayList<>();
+		allPredicates.add(p -> p.getProductId().equals(productId));
+		allPredicates.add(p -> p.getBrandId().equals(brandId));
+		allPredicates.add(p -> p.getStartDate().before(date));
+		allPredicates.add(p -> p.getEndDate().after(date));
 
-		Optional<Price> priceOpt;
-		try {
-			priceOpt = ondateRepository.findFirstByProductIdAndBrandIdAndStartDateBeforeAndEndDateAfterOrderByPriorityDesc(productId,brandId,date,date);			
-		}catch(DataAccessException e) {
-			log.error("Error in database query : {}",e.getMessage());
-			throw e;
-		}
-		
-		if(priceOpt.isPresent()) {			
-			Price price = priceOpt.get();
-			response = ondateMapper.priceToOndateResponse(price);
-		}
-		Optional<OndateResponse> responseOpt = Optional.ofNullable(response);
+		Optional<Price> price = priceList.stream()
+				.filter(allPredicates.stream().reduce(x -> true, Predicate::and))
+				.sorted((p1, p2) -> p2.getPriority().compareTo(p1.getPriority()))
+				.findFirst();
 
-		return responseOpt;
+		OndateResponse result = ondateMapper.priceToOndateResponse(price.orElse(null));
+
+		return Optional.ofNullable(result);
 	}
+
+//	@Override
+//	public Optional<OndateResponse> getPrice(Date date, Integer productId, Integer brandId) {
+//
+//		OndateResponse response = null;
+//
+//		Optional<Price> priceOpt;
+//		try {
+//			priceOpt = ondateRepository.findFirstByProductIdAndBrandIdAndStartDateBeforeAndEndDateAfterOrderByPriorityDesc(productId,brandId,date,date);
+//		}catch(DataAccessException e) {
+//			log.error("Error in database query : {}",e.getMessage());
+//			throw e;
+//		}
+//
+//		if(priceOpt.isPresent()) {
+//			Price price = priceOpt.get();
+//			response = ondateMapper.priceToOndateResponse(price);
+//		}
+//		Optional<OndateResponse> responseOpt = Optional.ofNullable(response);
+//
+//		return responseOpt;
+//	}
 
 }
